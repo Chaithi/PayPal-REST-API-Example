c9f4a005576205858657555ac6968709ed74c2e0
Rob <kitsucoon@gmail.com>
2016-03-21 16:18:48 -0500

    Update doexpress.php

--- doexpress.php
+++ doexpress.php
@@ -1,13 +1,10 @@
-<!-- 
-
-Developed by Robert Thayer, GamerGadgets.net
-Contact: rob@gamergadgets.net
-Simple Script using the PayPal REST API for an express checkout payment.
-You may use this code as you see fit. I do not offer support with this script.
-
--->
-
 <?php
+    /*
+    Developed by Robert Thayer, GamerGadgets.net
+    Contact: rob@gamergadgets.net
+    Simple Script using the PayPal REST API for an express checkout payment.
+    You may use this code as you see fit. I do not offer support with this script.
+    */
     // Take a payID and Payer ID from return.php and confirm the payment.
     include_once('restfunctions.php');
     $payerID = $_POST['payerId'];

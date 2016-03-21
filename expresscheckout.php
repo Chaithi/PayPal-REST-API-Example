788140274c120aafee64d7e225a4fd59d102a662
Rob <kitsucoon@gmail.com>
2016-03-21 16:20:39 -0500

    Update expresscheckout.php

--- expresscheckout.php
+++ expresscheckout.php
@@ -1,14 +1,10 @@
-<!-- 
-
-Developed by Robert Thayer, GamerGadgets.net
-Contact: rob@gamergadgets.net
-Simple Script using the PayPal REST API for an express checkout payment.
-You may use this code as you see fit. I do not offer support with this script.
-
--->
-
 <?php
-
+    /*
+    Developed by Robert Thayer, GamerGadgets.net
+    Contact: rob@gamergadgets.net
+    Simple Script using the PayPal REST API for an express checkout payment.
+    You may use this code as you see fit. I do not offer support with this script.
+    */
 // Script takes a payment amount from index.html and creates a PayPal payment for that amount.
     include_once('restfunctions.php');
     $amount = $_POST['amount'];
